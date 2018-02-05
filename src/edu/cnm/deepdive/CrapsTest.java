850c9f812393893636b467fd2ebaefa013190475
Freddie <freddie.romero23@yahoo.com>
2018-02-05 16:53:55 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/CrapsTest.java
@@ -0,0 +1,38 @@
+package edu.cnm.deepdive;
+
+import edu.cnm.deepdive.Craps.State;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CrapsTest {
+
+  Craps craps;
+
+  @BeforeEach
+  void setUp() {
+    craps = new Craps();
+    Assertions.assertNotNull(craps);
+  }
+
+  @Test
+  void play() {
+    Assertions.assertEquals(0, craps.getRolls().size());
+    for (int i = 0; i < 30; i++) {
+    Craps.State state = craps.play();
+    List<int[]> rolls = craps.getRolls();
+    Assertions.assertNotEquals(0, rolls.size());
+    Assertions.assertTrue(state == Craps.State.WIN || state == Craps.State.LOSS);
+    Assertions.assertTrue(
+        state != Craps.State.WIN
+        || rolls.size() > 1
+        || rolls.get(0)[0] + rolls.get(0)[1] == 7
+        || rolls.get(0)[0] + rolls.get(0)[1] == 11
+    );
+    Assertions.assertTrue(
+        state != Craps.State.WIN
+        || rolls.size() == 1
+    )
+  }
+}
---
+++ src/edu/cnm/deepdive/Play.java
@@ -0,0 +1,35 @@
+package edu.cnm.deepdive;
+
+import java.util.Scanner;
+
+public class Play {
+
+  public static void main(String[] args) {
+    int autoPlays = (args.length > 0) ? Integer.parseInt(args[0]) : 0;
+    Craps craps = new Craps();
+    Scanner scanner = new Scanner(System.in);
+    int wins = 0;
+    int plays = 0;
+    do {
+      plays++;
+      Craps.State result = craps.play();
+      if (result == Craps.State.WIN) {
+        wins++;
+      }
+      if (plays >= autoPlays) {
+        for (int[] dice : craps.getRolls()) {
+          System.out.printf("%d %d%n", dice[0], dice[1]);
+        }
+        System.out.println(result);
+        System.out.printf("Plays: %d. Wins: %d. Winning percentage: %.2f%%%n",
+            plays, wins, 100.0 * wins / plays);
+        System.out.println("Play Again? [Y/n]");
+        String input = scanner.nextLine().trim().toLowerCase();
+        if (!input.isEmpty() && input.charAt(0) == 'n') {
+          break;
+        }
+      }
+    } while (true);
+  }
+
+}
